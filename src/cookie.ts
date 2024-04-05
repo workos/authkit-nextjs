65ad61c36bf176a5ae63929c2efc7b63f760d716
Benoît Grélard <benoit.grelard@gmail.com>
2024-04-05 13:12:04 +0100

    Fix cookies not setting on Safari on localhost (#25)

--- src/cookie.ts
+++ src/cookie.ts
@@ -1,8 +1,13 @@
+import { WORKOS_REDIRECT_URI } from './env-variables.js';
+
+const redirectUrl = new URL(WORKOS_REDIRECT_URI);
+const isSecureProtocol = redirectUrl.protocol === 'https:';
+
 const cookieName = 'wos-session';
 const cookieOptions = {
   path: '/',
   httpOnly: true,
-  secure: true,
+  secure: isSecureProtocol,
   sameSite: 'lax' as const,
 };
 
