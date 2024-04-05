e66c91c18e1ce4db374aea9c7eada434c76d9830
Paul Asjes <108872335+PaulAsjes@users.noreply.github.com>
2024-04-05 13:27:09 +0200

    Update src/get-authorization-url.ts
    Co-authored-by: Benoît Grélard <benoit.grelard@gmail.com>

--- src/get-authorization-url.ts
+++ src/get-authorization-url.ts
@@ -2,7 +2,7 @@ import { workos } from './workos.js';
 import { WORKOS_CLIENT_ID, WORKOS_REDIRECT_URI } from './env-variables.js';
 import { GetAuthURLOptions } from './interfaces.js';
 
-async function getAuthorizationUrl(options: GetAuthURLOptions) {
+async function getAuthorizationUrl(options: GetAuthURLOptions = {}) {
   const { returnPathname, screenHint } = options;
 
   return workos.userManagement.getAuthorizationUrl({
