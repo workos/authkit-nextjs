eaf881f12cfacbfdd86d51114c63d0467e2328c7
Nir Tamir <nirtamir2@gmail.com>
2024-04-05 22:12:00 +0300

    export cookieOptions, cookieName and encryptSession from WorkOS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { handleAuth } from './authkit-callback-route.js';
 import { authkitMiddleware } from './middleware.js';
-import { getUser } from './session.js';
+import { getUser, encryptSession } from './session.js';
 import { getSignInUrl, getSignUpUrl, signOut } from './auth.js';
-import { cookieName } from './cookie.js';
+import { cookieName, cookieOptions } from './cookie.js';
 import { Impersonation } from './impersonation.js';
 
 export {
@@ -13,9 +13,11 @@ export {
   getSignInUrl,
   getSignUpUrl,
   getUser,
+  encryptSession,
   signOut,
   //
   cookieName,
+  cookieOptions,
   //
   Impersonation,
 };
