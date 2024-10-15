8673f47a5b994e0efe9c5a86676821d0d59c054f
Paul Asjes <paul.asjes@workos.com>
2024-10-15 05:11:40 -0400

    Fix bug with redirect URI (#108)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 import { NextMiddleware } from 'next/server';
 import { updateSession } from './session.js';
 import { AuthkitMiddlewareOptions } from './interfaces.js';
+import { WORKOS_REDIRECT_URI } from './env-variables.js';
 
 export function authkitMiddleware({
   debug = false,
   middlewareAuth = { enabled: false, unauthenticatedPaths: [] },
-  redirectUri = '',
+  redirectUri = WORKOS_REDIRECT_URI,
 }: AuthkitMiddlewareOptions = {}): NextMiddleware {
   return function (request) {
     return updateSession(request, debug, middlewareAuth, redirectUri);
