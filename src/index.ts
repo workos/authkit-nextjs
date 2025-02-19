dea8d0eff69e9c97c1c7b384da31900f224071b9
Nick Nisi <nick.nisi@workos.com>
2025-02-19 14:38:24 -0600

    expose the getWorkOS function in the module exports
    Allow users direct access to the WorkOS instance

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { handleAuth } from './authkit-callback-route.js';
 import { authkit, authkitMiddleware } from './middleware.js';
 import { withAuth, refreshSession } from './session.js';
 import { getSignInUrl, getSignUpUrl, signOut } from './auth.js';
+import { getWorkOS } from './workos.js';
 
 export * from './interfaces.js';
 
 export {
+  getWorkOS,
   handleAuth,
   //
   authkitMiddleware,
