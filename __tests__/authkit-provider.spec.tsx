ef39f3f977258c438e9bd26b12dee65fceb9e5ac
Nick Nisi <nick.nisi@workos.com>
2025-02-21 15:03:04 -0600

    remove oauthTokens from mock in test

--- __tests__/authkit-provider.spec.tsx
+++ __tests__/authkit-provider.spec.tsx
@@ -219,7 +219,6 @@ describe('useAuth', () => {
       permissions: ['read', 'write'],
       entitlements: ['feature1'],
       impersonator: { email: 'admin@example.com' },
-      oauthTokens: { access_token: 'token123' },
     });
 
     const TestComponent = () => {
