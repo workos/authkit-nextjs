d70b3c1ae12964db8c21a23c0a857305e7a0c3c1
Hasan Kumar <mintuhouse@gmail.com>
2025-03-17 20:22:12 +0000

    Accept redirectUri as an argument

--- src/auth.ts
+++ src/auth.ts
@@ -5,12 +5,20 @@ import { cookies } from 'next/headers';
 import { terminateSession } from './session.js';
 import { WORKOS_COOKIE_NAME, WORKOS_COOKIE_DOMAIN } from './env-variables.js';
 
-async function getSignInUrl({ organizationId, loginHint }: { organizationId?: string; loginHint?: string } = {}) {
-  return getAuthorizationUrl({ organizationId, screenHint: 'sign-in', loginHint });
+async function getSignInUrl({
+  organizationId,
+  loginHint,
+  redirectUri,
+}: { organizationId?: string; loginHint?: string; redirectUri?: string } = {}) {
+  return getAuthorizationUrl({ organizationId, screenHint: 'sign-in', loginHint, redirectUri });
 }
 
-async function getSignUpUrl({ organizationId, loginHint }: { organizationId?: string; loginHint?: string } = {}) {
-  return getAuthorizationUrl({ organizationId, screenHint: 'sign-up', loginHint });
+async function getSignUpUrl({
+  organizationId,
+  loginHint,
+  redirectUri,
+}: { organizationId?: string; loginHint?: string; redirectUri?: string } = {}) {
+  return getAuthorizationUrl({ organizationId, screenHint: 'sign-up', loginHint, redirectUri });
 }
 
 async function signOut({ returnTo }: { returnTo?: string } = {}) {
