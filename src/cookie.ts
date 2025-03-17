4668a8f3d9337ee67faf7c8548ab25e6254ed10c
Nick Nisi <nick.nisi@workos.com>
2025-03-17 16:31:15 -0500

    add WORKOS_COOKIE_SAMESITE optional configuration (#218)
    * add WORKOS_COOKIE_SAMESITE optional configuration
    
    * add tests around sameSite config
    
    * update README
    
    * Update README.md

--- src/cookie.ts
+++ src/cookie.ts
@@ -1,5 +1,18 @@
-import { WORKOS_REDIRECT_URI, WORKOS_COOKIE_MAX_AGE, WORKOS_COOKIE_DOMAIN } from './env-variables.js';
+import {
+  WORKOS_REDIRECT_URI,
+  WORKOS_COOKIE_MAX_AGE,
+  WORKOS_COOKIE_DOMAIN,
+  WORKOS_COOKIE_SAMESITE,
+} from './env-variables.js';
 import { CookieOptions } from './interfaces.js';
+
+type ValidSameSite = CookieOptions['sameSite'];
+
+function assertValidSamSite(sameSite: string): asserts sameSite is ValidSameSite {
+  if (!['lax', 'strict', 'none'].includes(sameSite.toLowerCase())) {
+    throw new Error(`Invalid SameSite value: ${sameSite}`);
+  }
+}
 
 export function getCookieOptions(): CookieOptions;
 export function getCookieOptions(redirectUri?: string | null): CookieOptions;
@@ -20,16 +33,19 @@ export function getCookieOptions(
   expired: boolean = false,
 ): CookieOptions | string {
   const url = new URL(redirectUri || WORKOS_REDIRECT_URI);
+  const sameSite = WORKOS_COOKIE_SAMESITE || 'lax';
+  assertValidSamSite(sameSite);
+  const secure = sameSite.toLowerCase() === 'none' ? true : url.protocol === 'https:';
 
   const maxAge = expired ? 0 : WORKOS_COOKIE_MAX_AGE ? parseInt(WORKOS_COOKIE_MAX_AGE, 10) : 60 * 60 * 24 * 400;
 
   return asString
-    ? `Path=/; HttpOnly; Secure=${url.protocol === 'https:'}; SameSite="Lax"; Max-Age=${maxAge}; Domain=${WORKOS_COOKIE_DOMAIN || ''}`
+    ? `Path=/; HttpOnly; Secure=${secure}; SameSite="${sameSite}"; Max-Age=${maxAge}; Domain=${WORKOS_COOKIE_DOMAIN || ''}`
     : {
         path: '/',
         httpOnly: true,
-        secure: url.protocol === 'https:',
-        sameSite: 'lax' as const,
+        secure,
+        sameSite,
         // Defaults to 400 days, the maximum allowed by Chrome
         // It's fine to have a long cookie expiry date as the access/refresh tokens
         // act as the actual time-limited aspects of the session.
