962b8357c721ae293864af3eb7d40d730db3a272
Nick Nisi <nick.nisi@workos.com>
2025-09-11 17:07:02 -0500

    reduce test coverage to 80% (#303)

--- jest.config.ts
+++ jest.config.ts
@@ -56,10 +56,10 @@ const config: Config = {
 
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
